023df0ce91bf982232475c7f5500373015cbfe64
Humus Walls <humusthewalls@gmail.com>
2016-09-23 02:03:53 +0000

    Bug-free skeleton finished.

--- Classes.py
+++ Classes.py
@@ -1,86 +1,122 @@
+import random
 
+from bookie import flag
 
 class Player:
-  
-  
-  def __init__(record):
+  def __init__(self, record):
     if type(record[0]) is not str:
       raise InitError(str(name)+" is not a valid name.")
     self.name = record[0]
     self.matches = record[1] if len(record)>1 else [] # matches are tuples of [opponent_rating, result]
+    if flag["simulate"]:
+      self.strength = random.random() # in simulations, estimates chances of winning
+    else:
+      self.strength = record[2] if len(record)>2 else .5 # can be calculated based on record
+    self.rating = 1000
     self.calculate_standing()
   
-  def calculate_standing():
+  # Allows class to be sorted by rating
+  def __lt__(self, other):
+    return other.rating < self.rating
+  
+  def calculate_standing(self):
+    # Calculates a player's total standing
+    # from scratch every time
+    # based on player's self.matches record
     self.wins = 0
     self.losses = 0
     self.ties = 0
-    self.total_opponent_rating = 0
     for match in self.matches:
-      self.total_opponent_rating += match[0]
-      if match[1] is True:
-        self.wins += 1
-      elif match[1] is False:
-        self.losses += 1
-      else:
-        self.ties += 1
-    if len(self.matches):
-      self.rating = (self.total_opponent_rating + 400(self.wins-self.losses))/len(self.matches)
-    else:
-      self.rating = 1000
+      # ELO rating system
+      self.adjust_rating()
+  
+  def adjust_rating(self, match=None):
+    # adjusts rating according to individual match
+    # using a 'match' in form of [rating, outcome]
+    if match is None:
+      match = self.matches[-1]
+    win_probability = 1/(pow(10,(abs(match[0]-self.rating)/400))+1)
+    point_exchange = 0
+    if (self.rating <= match[0] and match[1] is True) or (self.rating >= match[0] and match[1] is False):
+      # Underdog and wins
+      # or Expected and loses
+      point_exchange = int(32*(1 - win_probability)) # PE(u)
+    elif (self.rating >= match[0] and match[1] is True) or (self.rating <= match[0] and match[1] is False):
+      ######### "or equal" because tied ratings work with either equation
+      # Underdog/Even and loses
+      # or Expected/Even and wins
+      point_exchange = int(32*win_probability) # PE(e)
+    else: # should only occur for match[1] is None: a tie
+      point_exchange = int(16*(1-2*win_probability)) # simplification of (PE(u) - PE(e))/2
+    if flag["loud"]:
+      print("    {0}pts ({1} vs {2})-{3:.0f}%".format(point_exchange,self.rating,match[0],100*win_probability))
+    if match[1] is True: # Won
+      self.rating += point_exchange
+      self.wins += 1
+    elif match[1] is False: # Lost
+      self.rating -= point_exchange
+      self.losses += 1
+    else: # Tied
+      # points are either gained or lost depending on difference between self and opponent ratings
+      self.rating = (self.rating + point_exchange) if self.rating < match[0] else (self.rating - point_exchange)
+      self.ties += 1
   
   def add_match(self, opponent_rating, outcome):
     if type(opponent_rating) is not int or type(outcome) is not bool:
       raise ChessError("Invalid match passed: "+str(outcome)+": "+str(opponent_rating))
     self.matches.append([opponent_rating, outcome])
-    self.calculate_standing()
   
-  def report(self, requested="name"):
+  def report(self, requested="summary"):
     string = ""
     if requested == "name":
       string += self.name
+    elif requested == "summary":
+      string += "("+str(self.rating)+":{0:.2f}".format(self.strength)+") "+self.name
+      string += " W-"+str(self.wins)+" L-"+str(self.losses)+" T-"+str(self.ties)
     elif requested == "rating":
-      return self.rating
+      return int(self.rating)
     elif requested == "matches":
       for match in self.matches:
         string += str(match[0])+" "+str(match[1])+","
-        string = string[:-1] # remove final ","
+      string = string[:-1] # remove final ","
+    elif requested == "strength":
+      return float(self.strength)
     elif requested == "wins":
-      return self.wins
+      return int(self.wins)
     elif requested == "losses":
-      return self.losses
+      return int(self.losses)
     elif requested == "ties":
-      return self.ties
+      return int(self.ties)
     elif requested == "file":
       string += self.report("name")+";"
       string += self.report("matches")
+    else:
+      string += "ARG AN ERROR KILL IT WITH FIRE!"
     return string
 
 class Match:
-  
-  
-  @staticmethod
-  def check_players(player_list):
-    if type(player_list) is not list:
-      raise InitError(str(player_list)+" is not a valid list of players.")
-    if len(player_list) is not 2:
-      raise InitError(str(player_list)+" is "+("not long enough." if len(player_list)<2 else "too long."))
+  def check_players(self):
+    if type(self.players) is not list:
+      raise InitError(str(self.players)+" is not a valid list of players.")
+    if len(self.players) is not 2:
+      raise InitError(str(self.players)+" is "+("not long enough." if len(self.players)<2 else "too long."))
   
   @staticmethod
   def set_ratings(players, outcome):
     # 2-player game, standard outcome resolution per ELO
-    player[0].add_match(player[1].report("rating"), outcome)
-    player[1].add_match(player[0].report("rating"), outcome)
+    players[0].add_match(players[1].report("rating"), outcome)
+    players[1].add_match(players[0].report("rating"), (not outcome) if outcome is not None else outcome)
+    players[0].adjust_rating()
+    players[1].adjust_rating()
   
-  def __init__(player_list, outcome="Nope"):
-    check_players(player_list)
+  def __init__(self, player_list, outcome="Nope"):
     self.players = player_list
+    self.check_players()
     self.result = outcome
-    if type(self.result) is bool:
+    if type(self.result) is bool or self.result is None:
       self.resolve(self.result)
   
   def resolve(self, outcome):
-    if type(outcome) is not bool:
-      raise ChessError("No valid result was passed.")
     self.result = outcome
       ##############################
      # Tell players what happened #
@@ -93,13 +129,38 @@ class Match:
       for player in self.players:
         string += player.report("name")+", "
       string = string[:-2] # remove last ", "
+    elif requested == "players":
+      return self.players
     elif requested == "result":
       return self.result
     elif requested == "file":
       string += self.report("name")+";"
       string += str(self.report("result"))
+    else:
+      string += "ARG AN ERROR KILL IT WITH FIRE!"
     return string
 
+class BBMatch(Match):
+  def check_players(self):
+    if type(self.players) is not list:
+      raise InitError(str(self.players)+" is not a valid list of players.")
+    if len(self.players) is not 4:
+      raise InitError(str(self.players)+" is "+("not long enough." if len(self.players)<4 else "too long."))
+    # splits list of players into teams, and places higher ranked player of team on top
+    # then rejoins list in sorted (T1H, T1L, T2H, T2L) form
+    self.players = self.players[:1].sort().append(self.players[2:].sort())
+  
+  @staticmethod
+  def set_Ratings(players, outcome):
+    # 4-player 'bug-out' chess match
+    # Higher-rated winner matched with lower-rated loser
+    # And visa-versa
+    # players should be in known sorted form
+    # as per check_players() sorting
+    players[0].add_match(players[3].report("rating"), outcome)
+    players[1].add_match(players[2].report("rating"), outcome)
+    players[2].add_match(players[1].report("rating"), (not outcome) if outcome is not None else None)
+    players[3].add_match(players[0].report("rating"), (not outcome) if outcome is not None else None)
 
 class ChessError(Warning):
   def __init__(self, string="An unidentified Chess Error has occurred!"):
--- bookie.py
+++ bookie.py
@@ -1,6 +1,6 @@
 # Bug-Board Official Automated Rating Database
 import sys
-from Classes import *
+import random
 
 
     ########
@@ -8,16 +8,75 @@ from Classes import *
   ########
 # Oh god so much
 
-
+# Flags
+flag = {"simulate" : False,
+        "loud"     : False}
+from Classes import *
 
 def do_things():
+  if flag["simulate"]:
+    matches = run_games(create_players(20), 1000)
+    display_results(matches)
+    pass
   pass
 
+def create_players(count):
+  player_list = []
+  if flag["loud"]:
+    print("Players:")
+  for i in range(count):
+    player_list.append(Player([str("Player "+str(i))]))
+    if flag["loud"]:
+      print("{0} at strength {1:.2f}".format(player_list[-1].report("name"),player_list[-1].report("strength")))
+  return player_list
+
+def run_games(players, game_count):
+  match_list = []
+  if flag["loud"]:
+    print("") # separating line for clarity
+  for i in range(game_count):
+    playing = random.sample(players, 2)
+    playing.sort()
+    if flag["loud"]:
+      print(playing[0].report("name")+" vs "+playing[1].report("name"))
+    win_chance = .5+(playing[0].report("strength")-playing[1].report("strength"))
+    outcome = True if random.random() < win_chance else False
+    if flag["loud"]:
+      print("   - "+playing[0].report("name")+" "+("wins!" if outcome else "tied!" if outcome is None else "loses!"))
+      print("     ({0:.2f}-{1:.2f}) = {2:.2f}".format(playing[0].report("strength"),playing[1].report("strength"),win_chance))
+      print("  - Match Summary:")
+    match = Match(playing, outcome)
+    match_list.append(match)
+  return match_list
+
+def display_results(match_list):
+  player_list = []
+  for match in match_list:
+    players = match.report("players")
+    for player in players:
+      try:
+        player_list.index(player)
+      except ValueError:
+        player_list.append(player)
+  player_list.sort()
+  if not flag["loud"]:
+    total_points = 0
+    for player in player_list:
+      total_points += player.report("rating")
+      print(player.report())
+    print("Points per player: {0:.0f}".format(total_points/len(player_list)))
 
 
 if __name__ == "__main__":
   try:
     commands = sys.argv[1:]
+    command_count = 0
+    for command in commands:
+      if command in flag:
+        flag[command] = True
+        command_count += 1
+      else:
+        raise ChessError("Invalid command sent: "+command)
     do_things()
   except KeyboardInterupt as KI:
     print("~~~~~~~~~~~~~~~~~~~~~~Ok Bye!~~~~~~~~~~~~~~~~~~~")
